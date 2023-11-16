476bc8069f088e461afe4e70130b847ae0f8329c
asafi67 <asafi1@mail.sfsu.edu>
2023-11-15 23:06:05 -0800

    basic image slideshow

--- script.js
+++ script.js
@@ -78,3 +78,18 @@ function closePopup() {
     document.getElementById("popup").style.display = "none";
 
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+} 
